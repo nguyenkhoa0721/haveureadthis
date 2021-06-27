dd49efb8b86be8524104331b23fc035bdc071746
ngkhoa <nguyenkhoa0721@gmail.com>
2021-06-27 16:50:08 +0700

    deploy

--- src/main.js
+++ src/main.js
@@ -2,9 +2,10 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
-import axios from 'axios';
+import axios from 'axios'
+import VueAxios from 'vue-axios'
 
-Vue.use(axios);
+Vue.use(VueAxios, axios)
 axios.defaults.baseURL = 'http://api.haveyoureadthis.tech'
 axios.defaults.withCredentials = true;
 
