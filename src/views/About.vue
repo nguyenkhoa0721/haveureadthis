5195d5bb7ff4bc9f926ab76b64c10e2889ebfd07
ngkhoa <nguyenkhoa0721@gmail.com>
2021-06-28 00:42:51 +0700

    add button

--- src/views/About.vue
+++ src/views/About.vue
@@ -4,6 +4,14 @@
       <div class="container has-text-centered">
         <p class="title">Công trình thành niên CNTN19</p>
         <p class="subtitle">It's Not a Bug, It's a Feature</p>
+        <a
+          class="subtitle"
+          href="https://docs.google.com/forms/d/e/1FAIpQLScjMD6bYCfV2dJvMEa_XsizFPPIouSggxTnSco0f-vH8n7gIg/viewform"
+          target="chart"
+          ><button class="button is-danger is-hovered is-small">
+            Đóng góp cho HURTT
+          </button></a
+        >
       </div>
     </div>
   </section>
