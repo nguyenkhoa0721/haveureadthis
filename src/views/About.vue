fd1d007b059a369c3712e62c995f977302f58ca6
ngkhoa <nguyenkhoa0721@gmail.com>
2021-06-28 00:26:41 +0700

    about

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,10 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
-  </div>
+  <section class="hero is-success is-fullheight-with-navbar">
+    <div class="hero-body">
+      <div class="container has-text-centered">
+        <p class="title">Công trình thành niên CNTN19</p>
+        <p class="subtitle">It's Not a Bug, It's a Feature</p>
+      </div>
+    </div>
+  </section>
 </template>
